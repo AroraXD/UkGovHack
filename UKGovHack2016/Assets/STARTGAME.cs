1563dc91a5604bb985613b7d157e64bd8939e98f
uveavanto <uve@Yuveshs-MacBook-Air.local>
2016-04-03 01:24:03 +0100

    deleted junk code

--- UKGovHack2016/Assets/STARTGAME.cs
+++ UKGovHack2016/Assets/STARTGAME.cs
@@ -20,21 +20,3 @@ public class STARTGAME : MonoBehaviour {
 		button.SetActive(false);
 	}
 }
-
-/*
-public BubbleController bubbleController;
-private bool isShowing;
-
-// Use this for initialization
-void Start () {
-	isShowing = false;
-}
-
-// Update is called once per frame
-void Update () {
-	if (Input.GetKeyDown("escape")) {
-		isShowing = !isShowing;
-	}
-
-	menu.SetActive(isShowing);
-	*/
